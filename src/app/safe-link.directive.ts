9872cdfc1571c7d8bd78df5d5727325338be2680
IDOMATH <btdothemath@gmail.com>
2025-03-20 17:48:18 -0500

    Adding safe link directive

---
+++ src/app/safe-link.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, input } from "@angular/core";
+
+@Directive({
+  selector: "a[appSafeLink]",
+  standalone: true,
+  host: { "(click)": "onDirectiveClicked($event)" },
+})
+export class SafeLinkDirective {
+  queryParam = input("myapp", { alias: "appSafeLink" });
+  constructor() {
+    console.log("SafeLinkDirective is active!");
+  }
+
+  onDirectiveClicked(event: MouseEvent) {
+    const wantsToLeave = window.confirm("Do you want to leave the app?");
+    if (wantsToLeave) {
+      const address = (event.target as HTMLAnchorElement).href;
+      (event.target as HTMLAnchorElement).href =
+        address + "?from=m" + this.queryParam();
+      return;
+    }
+    event.preventDefault();
+  }
+}
